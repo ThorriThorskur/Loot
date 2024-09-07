eee311b3041c08936269b83b27157458aea89b96
ThorriThorskur <73035171+ThorriThorskur@users.noreply.github.com>
2024-09-07 22:25:08 +0000

    initial commit

---
+++ src/test/java/is/hi/hbv501g/loot/LootApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.loot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
